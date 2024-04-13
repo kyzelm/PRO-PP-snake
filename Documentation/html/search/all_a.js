06030974302974c55c789690b14748904068b0ea
Damian Osinski <osinskidamian03@gmail.com>
2024-04-13 19:40:53 +0200

    Dox update

--- Documentation/html/search/all_a.js
+++ Documentation/html/search/all_a.js
@@ -4,7 +4,7 @@ var searchData=
   ['map_1',['Map',['../df/d91/struct_map.html',1,'Map'],['../d3/d63/_map_8h.html#afdfce14f0d12266a9956f9fe5e4fe1fd',1,'Map:&#160;Map.h']]],
   ['map_2ec_2',['Map.c',['../d0/dec/_map_8c.html',1,'']]],
   ['map_2eh_3',['Map.h',['../d3/d63/_map_8h.html',1,'']]],
-  ['maptable_4',['mapTable',['../df/d91/struct_map.html#ab52b42d06002cee06ef60246fea9d094',1,'Map']]],
+  ['maptable_4',['mapTable',['../df/d91/struct_map.html#aae9feb68441b2a09148d4ee0b0040471',1,'Map']]],
   ['menu_5',['menu',['../d8/dfd/struct_interface_controller.html#a8493248c630088c793c70d9aa99b6e88',1,'InterfaceController']]],
   ['menucursor_6',['menuCursor',['../d8/dfd/struct_interface_controller.html#a76923fcbbdedc091727d76535236442b',1,'InterfaceController']]],
   ['menumode_7',['menuMode',['../d8/dfd/struct_interface_controller.html#a65d41d4e04ee68c9f9726180492cac03',1,'InterfaceController']]],
--- Documentation/html/search/variables_6.js
+++ Documentation/html/search/variables_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['maptable_0',['mapTable',['../df/d91/struct_map.html#ab52b42d06002cee06ef60246fea9d094',1,'Map']]],
+  ['maptable_0',['mapTable',['../df/d91/struct_map.html#aae9feb68441b2a09148d4ee0b0040471',1,'Map']]],
   ['menu_1',['menu',['../d8/dfd/struct_interface_controller.html#a8493248c630088c793c70d9aa99b6e88',1,'InterfaceController']]],
   ['menucursor_2',['menuCursor',['../d8/dfd/struct_interface_controller.html#a76923fcbbdedc091727d76535236442b',1,'InterfaceController']]],
   ['menumode_3',['menuMode',['../d8/dfd/struct_interface_controller.html#a65d41d4e04ee68c9f9726180492cac03',1,'InterfaceController']]]
